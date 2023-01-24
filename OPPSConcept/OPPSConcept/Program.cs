ec527798c32829a494d74d80cf42e09e87e54628
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-24 15:45:50 +0530

    [prashant]add: Performing Inheritance

---
+++ OPPSConcept/OPPSConcept/Program.cs
@@ -0,0 +1,23 @@
+﻿using OPPSConcept.Inheritance;
+using System;
+namespace OPPSConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OPPS Concept");
+            Console.WriteLine("Plese Choose a Number to Perform a Program: ");
+            Console.WriteLine("1:Inheritance");
+            int Options = Convert.ToInt32(Console.ReadLine());
+            switch(Options)
+            {
+                case 1:
+                    Class1 class1= new Class1();
+                    class1.Test1();
+                    class1.Test2();
+                    break;
+            }
+        }
+    }
+}
